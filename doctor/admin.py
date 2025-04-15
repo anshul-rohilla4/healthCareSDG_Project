dd6688eb373a9bc52a17c7d427218512f76c0ca1
anshul-rohilla4 <anshulrohilla111@gmail.com>
2025-04-15 14:07:00 +0000

    basic error debug and url changes

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from django.contrib.admin.options import ModelAdmin
 from doctor.models import DoctorInfo
-# Register your models here.
+from django.contrib.auth.models import User
 
-admin.site.register(DoctorInfo)
+@admin.register(DoctorInfo)
+class DoctorInfoAdmin(admin.ModelAdmin):
+    list_display = ['get_name', 'department']
+    list_filter = ['department']
+    search_fields = ['user__first_name', 'user__last_name', 'department', 'user__username']
+    raw_id_fields = ['user']
+
+    def get_name(self, obj):
+        username = obj.user.username
+        first_name = obj.user.first_name or username
+        last_name = obj.user.last_name or ""
+        return f"Dr. {first_name} {last_name} ({username})"
+    get_name.short_description = 'Doctor Name'
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "user":
+            kwargs["queryset"] = User.objects.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
